ab81250ac5cb440b8c3ba5e73127493169727a31
aishwaryakamal <aishwaryakamal5@gmail.com>
2019-05-19 18:43:14 +0530

    Added all the packages

--- app/src/main/java/com/platinumstudio/searchgithubsamplemvvm/ui/MainActivity.java
+++ app/src/main/java/com/platinumstudio/searchgithubsamplemvvm/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.platinumstudio.searchgithubsamplemvvm;
+package com.platinumstudio.searchgithubsamplemvvm.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.platinumstudio.searchgithubsamplemvvm.R;
 
 public class MainActivity extends AppCompatActivity {
 
